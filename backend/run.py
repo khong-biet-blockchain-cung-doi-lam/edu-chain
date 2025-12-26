d91d13d39c1a78962cff0e4df8b4d69031e344e3
Bao-gio-co-tien <tranhong0347@gmail.com>
2025-12-26 22:39:23 +0700

    ua

--- backend/run.py
+++ backend/run.py
@@ -2,10 +2,14 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from flask import Flask
-from app import create_app
 from app.extensions import db
 from app.models.user_model import User
 from app.models.account_model import Account
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from app import create_app
 
 app = create_app()
 
@@ -14,4 +18,6 @@ def create_tables():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
+
+
