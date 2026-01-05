758a7f25a3725f22bd01e18d951804070231fd3f
Bao-gio-co-tien <tranhong0347@gmail.com>
2026-01-05 10:57:04 +0700

    sua lai ti khong dang ke

--- backend/app/models/student_model.py
+++ backend/app/models/student_model.py
@@ -3,7 +3,7 @@ from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 class Student(db.Model):
-    __tablename__ = 'students'
+    __tablename__ = 'student'
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     student_id = db.Column(db.String(20), unique=True, nullable=False)
