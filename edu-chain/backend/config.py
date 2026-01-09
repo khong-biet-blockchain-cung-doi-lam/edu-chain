c8f65f927e507a35ec6ebbb76cd2ba286201d98f
NguyenTienDat <wibukid@gmail.com>
2026-01-09 08:10:01 +0700

    Remove redundant edu-chain duplicate folder

--- edu-chain/backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'super-secret-key')
